3e6ee811c594561dd92c6a661e15b9e04042b2e0
sitiamalianurfaidah <150977479+sitiamalianurfaidah@users.noreply.github.com>
2025-05-04 18:49:25 +0700

    Perbaikan ke Sekian

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const PORT = process.env.PORT || 5000;
 const upload = require('./src/utils/upload.middleware'); 
 
 const corsOptions = {
-    origin: [
-        'http://localhost:5050', 
-        'https://twinklyfrontend-production.up.railway.app' 
-    ],
+    origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
